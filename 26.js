fdca722cbc7cfb72a0b17cd15601c6b2ff28ade9
Jeffrey Emerich Christian <15chrjef@gmail.com>
2017-01-19 17:23:44 -0800

    create 26.js

---
+++ 26.js
@@ -0,0 +1,19 @@
+var str = 'asdfjklasdlbjafsdlkjfadsljkdfajhkdsf';
+
+var strArr = str.split('').reverse().join('')
+
+var manualStringReversal = (string) => {
+  string = string.split('')
+  var middle = Math.floor(string.length / 2)
+  var start = 0;
+  var end = string.length - 1;
+  while(start !== middle) {
+    var temp = string[start];
+    string[start] = string[end];
+    string[end] = temp;
+    start ++;
+    end --;
+    console.log(string)
+  }
+  return string
+}
